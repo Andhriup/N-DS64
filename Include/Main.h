83a0e83c22236cedc683437df47b502055b5395e
Andhriup <154692258+Andhriup@users.noreply.github.com>
2026-02-25 23:22:11 -0400

    Add Main.h header file with constants and prototypes

---
+++ Include/Main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H   // Guarda para evitar que el archivo se incluya dos veces
+#define MAIN_H
+
+#include <nds.h> // Incluimos las librerías de la DS aquí para tenerlas en todo el proyecto
+
+// Definición de constantes para N$DS64
+#define VERSION "0.1a"
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 192
+
+// Prototipos de funciones (declaramos que existen)
+void inicializarConsola(void);
+void mostrarMenu(void);
+
+#endif
