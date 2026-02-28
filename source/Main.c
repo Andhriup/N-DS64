984294bb3dea1342207e05ace9f12bb0c1c91e94
Andhriup <molecula255@gmail.com>
2026-02-27 20:53:17 -0400

    update

--- source/Main.c
+++ source/Main.c
@@ -1,28 +1,18 @@
-#include <nds.h>
+#include <main.h>
 #include <stdio.h>
 
 int main(void) {
     consoleDemoInit();
-
-    // Seleccionar la pantalla superior para escribir
     consoleSelect(consoleGetDefault());
     printf("\n\n   N$DS64 Proyecto DSi\n");
     printf("   -------------------\n");
     printf("   Hola mundo!\n");
-
-    // Imprimir en la pantalla inferior (opcional)
     printf("\n\n   Presiona START para salir.");
-
     while(1) {
-        // Escuchar la entrada de 
         scanKeys();
         int keys = keysDown();
-
         if (keys & KEY_START) break;
-
-        // Esperar al refresco de pantalla (VBlank) para ahorrar batería
         swiWaitForVBlank();
     }
-
     return 0;
 }
