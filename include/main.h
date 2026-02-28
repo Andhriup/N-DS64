122283d152fa5a6df4051370023e80a2fb73192e
Andhriup <molecula255@gmail.com>
2026-02-28 12:26:46 -0400

    update

--- include/main.h
+++ include/main.h
@@ -1,6 +1,6 @@
+#include <nds.h>
 #ifndef MAIN_H   // Guarda para evitar que el archivo se incluya dos veces
 #define MAIN_H
-#include <nds.h> // Incluimos las librerías de la DS aquí para tenerlas en todo el proyecto
 
 // Definición de constantes para N$DS64
 #define VERSION "0.1a"
