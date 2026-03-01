638188d9a8b925838b0995f2c64ab801915cdc75
Andhriup <molecula255@gmail.com>
2026-03-01 11:09:39 -0400

    update

--- source/main.c
+++ source/main.c
@@ -1,22 +1,13 @@
 #include <nds.h>
 #include <stdio.h>
 #include "main.h"
-int __dsimode = 1;
-void __libnds_mpu_setup(void) {}
-void __libnds_exit(void) {}
-void* __secure_area__ = NULL;
 void initSystem(void) {}
 int main(void) {
     consoleDemoInit();
     printf("\n\n   N-DS64 Proyecto DSi\n");
     printf("   -------------------\n");
-    if (isDSiMode()) {
         printf("   Hardware: Nintendo DSi\n");
         printf("   Estado:   133MHz / 16MB RAM\n");
-    } else {
-        printf("   Hardware: Nintendo DS\n");
-        printf("   Estado:   67MHz / 4MB RAM\n");
-    }
 
     printf("\n\n   Presiona START para salir.");
 
