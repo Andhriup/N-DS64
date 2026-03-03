6698a876923f821d395a3e46c73c41a7eb912dee
Andhriup <molecula255@gmail.com>
2026-03-03 08:39:54 -0400

    update

--- source/main.cpp
+++ source/main.cpp
@@ -13,7 +13,6 @@ extern "C" {
     void __libnds_mpu_setup(void) { }
     void __libnds_exit(void) { }
     void initSystem(void) {
-      irqInit();
       irqEnable(IRQ_VBLANK);
       irqSet(IRQ_VBLANK, Vblank);
     }
